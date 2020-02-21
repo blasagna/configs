5d393e79097fd95f26dc8f18d21944314d5b4606
Bob DiMaiolo <rdimaiolo@fb.com>
2020-02-21 10:03:49 -0500

    powershell functions and aliases

---
+++ profile.ps1
@@ -0,0 +1,23 @@
+Function New-File {New-Item -ItemType file @args}
+New-Alias -Name "touch" -Value New-File
+
+Function Clear {clear}
+New-Alias -Name "cl" -Value Clear
+
+Function Git-Fetch {git fetch}
+Function Git-Pull {git pull}
+Function Git-Status {git status}
+Function Git-Log {git log --graph --decorate --oneline}
+Function Git-Branch {git branch @args}
+Function Git-Merge {git merge @args}
+Function Git-Commit {git commit @args}
+Function Git-Checkout {git checkout @args}
+New-Alias -Name "ggf" -Value Git-Fetch
+New-Alias -Name "ggp" -Value Git-Pull
+New-Alias -Name "ggs" -Value Git-Status
+New-Alias -Name "ggl" -Value Git-Log
+New-Alias -Name "ggb" -Value Git-Branch
+New-Alias -Name "ggm" -Value Git-Merge
+New-Alias -Name "ggc" -Value Git-Commit
+New-Alias -Name "ggco" -Value Git-Checkout
+
